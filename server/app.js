d4daf8a2740eb7f85db540b1a9aae4b45de564ea
Shawn <shawnmclau@gmail.com>
2017-03-28 01:08:45 -0400

    updated project for Angular 2 setup

--- server/app.js
+++ server/app.js
@@ -44,6 +44,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../client')));
+app.use(express.static(path.join(__dirname, '../node_modules')));
 
 // setup session
 app.use(session({
